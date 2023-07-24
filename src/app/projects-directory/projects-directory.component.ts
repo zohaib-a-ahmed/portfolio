f91c4d157240400c2c2919d9f56c3e5317d2386f
Zohaib Ahmed <zohaib0718@gmail.com>
2023-07-23 22:18:48 -0500

    Projects Page + Directed Links

--- src/app/projects-directory/projects-directory.component.ts
+++ src/app/projects-directory/projects-directory.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-projects-directory',
   templateUrl: './projects-directory.component.html',
-  styleUrls: ['./projects-directory.component.css']
+  styleUrls: ['./projects-directory.component.css'],
 })
 export class ProjectsDirectoryComponent {
-
+  projects: any[] = [
+    {
+      title: 'Atlas',
+      subtitle: 'AI Fitness Trainer',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam velit dui, posuere   eu, suscipit vehicula ex. Mauris scelerisque diam vel scelerisque sollicitudin. Maecenas magna turpis, tempus quis fermentum vel, rutrum quis nulla. Ut congue nisl et dui laoreet, sed ullamcorper arcu rutrum. Donec nec egestas lorem, quis pretium mi. Ut aliquet sed orci id consequat. Nulla suscipit enim eget neque iaculis, et porta nisi pellentesque. Proin a dignissim lacus.',
+      image: 'assets/projects/atlas.png',
+      flip: false,
+    },
+    {
+      title: 'Billiken Bot',
+      subtitle: 'SLU Slackbot Assistant',
+      description:
+        'In vitae magna diam. Aenean ultricies metus non sapien vestibulum, vel tristique justo pulvinar. Maecenas vel enim malesuada urna bibendum iaculis. Vivamus tempus, purus ac venenatis vehicula, sem dolor pharetra nibh, ac egestas ex sapien sed orci. Ut odio neque, pellentesque a tempor id, viverra porttitor velit. Vivamus sagittis nulla ante, id dictum est facilisis sit amet. Aliquam ut semper metus. Nam sit amet sapien vel nunc feugiat ornare sit amet eu leo. Proin blandit elementum laoreet. Etiam pretium aliquet arcu nec lacinia. Etiam imperdiet feugiat tempus. Sed ut egestas augue. Proin vitae velit diam. Suspendisse quam augue, consequat eget arcu id, luctus mattis nibh. Duis finibus, felis in congue mollis, arcu lacus ornare justo, quis convallis ante quam ac lorem. Aliquam dignissim in ligula nec feugiat.',
+      image: 'assets/projects/slackchat.jpeg',
+      flip: true,
+    },
+  ];
 }
