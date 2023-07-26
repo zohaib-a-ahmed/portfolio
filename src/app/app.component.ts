1646a24aa9276eff8591254f068b4a86d012cd81
Zohaib Ahmed <zohaib0718@gmail.com>
2023-07-25 20:41:54 -0500

    Added Font

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <main>
-      <section class="content">
+      <section class="content font-sans">
         <router-outlet></router-outlet>
       </section>
       <app-footer></app-footer>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
     content: [
       './src/app/home/navigation/navigation.component.html',
       './src/app/home/about/about.component.html',
       './src/app/home/projects/projects.component.html',
       './src/app/projects/project-directory.component.html',
-      './src/app/footer/footer.component.html'
+      './src/app/footer/footer.component.html',
+      './src/app.component.ts'
     ],
     theme: {
       extend: {
+        fontFamily: {
+          'sans': ['Avenir', ...defaultTheme.fontFamily.sans],
+        },
     },
     plugins: [],
   }
